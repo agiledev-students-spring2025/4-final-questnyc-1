0924bab1c8305549dd0a7265890019b4b9c1b82c
shiwen59 <135702207+shiwenz59@users.noreply.github.com>
2025-03-10 11:58:29 -0400

    completed quest page linked

--- front-end/src/pages/ProfilePage.js
+++ front-end/src/pages/ProfilePage.js
@@ -62,8 +62,8 @@ function ProfilePage() {
             {/* Action Buttons */}
             <div style={{ marginTop: '20px' }}>
                 <button style={buttonStyle}>Achievements</button>
-                <button style={buttonStyle}>Completed Quests</button>
-                <button style={buttonStyle} onClick={() => navigate('/friends-list-page')}>Friends List</button> {/* ✅ Navigates to Friends List */}
+                <button style={buttonStyle} onClick={() => navigate('/completed-quests')}>Completed Quests</button>
+                <button style={buttonStyle} onClick={() => navigate('/friends-list-page')}>Friends List</button>
                 <button style={buttonStyle}>Change Password</button>
             </div>
 
