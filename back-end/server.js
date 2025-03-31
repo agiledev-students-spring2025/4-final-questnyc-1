45376a2c23536d7c1cbea90f31e63cdb3234a663
IsaacProgrammer0 <it2197@nyu.edu>
2025-03-31 18:10:24 +0100

    fixed server.js package.json

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 const app = express()
+const PORT = process.env.PORT || 5000
+
 import path from 'path'
 import { fileURLToPath } from 'url'
 
