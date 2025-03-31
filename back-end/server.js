de9f82ac26c3298783da3ba39798d1241d46a3e6
IsaacProgrammer0 <it2197@nyu.edu>
2025-03-31 17:44:40 +0100

    added cors to server

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 // Middleware to parse JSON bodies
 app.use(express.json());
 
